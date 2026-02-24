4550535a293bae7dc9b326bac555633d28e93417
Omar Mohamed <om235@njit.edu>
2026-02-23 22:00:49 -0500

    change header to daily memory check

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # log the date and memory usage
 
-echo "Memory Log - $(date)" >> system_log.txt
+echo "DAILY Memory CHECK - $(date)" >> system_log.txt
 free -h | grep Mem >> system_log.txt
 echo "----------------------------" >> system_log.txt
